25e9f6d0e6faa1c4bcd6a8ed5566ea31c4325a00
Sazzadur Rahman Sujon <rahmansujon94@gmail.com>
2018-10-20 22:45:21 +0600

    newMessage create

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
 socket.on('connect', function() {
     console.log('connect to server');
 
-    socket.emit('createMessage', {
-        from: 'sujon',
-        text: 'hey this is sr'
-    });
+    
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -13,16 +13,16 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-
-    socket.emit('newMessage', {
-        from: 'sujonrahman',
-        text: 'hi this is sujon',
-        createAt: 123
-    });
+    console.log('New user connected');    
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
