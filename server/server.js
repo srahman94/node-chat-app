a75c919628770a4b76fb7bb7caf46b40d0c441be
Sazzadur Rahman Sujon <rahmansujon94@gmail.com>
2018-10-20 23:46:03 +0600

    greet new user and alert

--- server/server.js
+++ server/server.js
@@ -13,7 +13,18 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');    
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
@@ -21,8 +32,14 @@ io.on('connection', (socket) => {
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
-            createAt: new Date().getTime()
-        })
+            createdAt: new Date().getTime()
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
